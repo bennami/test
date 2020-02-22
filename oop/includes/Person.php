39e8cb27d25ac9a0b1c392b27959b24a32aa77f7
imane <benbenimane@hotmail.com>
2020-02-23 00:01:20 +0100

    watch about php things

---
+++ oop/includes/Person.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
+
+class Person{
+
+    Private $name;
+    Private $eyeColor;
+    Private $age;
+
+    //public static $drinkingAge = 18;
+    public static $fingers = 10;
+
+
+    public function __construct($name,$eyeColor,$age){
+    $this->name=$name;
+    $this->eyeColor=$eyeColor;
+    $this->age=$age;
+    }
+
+    public static function setFingers($howMany){
+         return self::$fingers = $howMany;
+    }
+
+   /* public function getFingers(){
+        return self::$fingers;
+    }*/
+
+    public function getName(){
+        return $this->name;
+    }
+}
+
+
+
+
---
+++ oop/index.php
@@ -0,0 +1,34 @@
+<?php
+require 'includes/Person.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>PHP</title>
+</head>
+<body>
+<?php
+
+//RECAP CLASS OBJECT
+//$object = new Person('tim','blue','30');
+//echo $object->getName();
+
+//STATIC PROPERTIES
+//echo Person::$drinkingAge;
+
+//STATIC FUNCTIONS
+//echo 'In 2020 we use to have '. Person::$fingers. ' fingers';
+//echo '<br>';
+//echo 'Now, it is the year 3290 and we have'.Person::setFingers(15).' fingers, we are using them well.';
+
+//STATIC, BUT WE CAN USE THEM AS NORMAL PROPERTIES (make it private)
+//$object = new Person('tim','blue','30');
+//$object->getFingers();
+
+
+
+
+?>
+</body>
+</html>
